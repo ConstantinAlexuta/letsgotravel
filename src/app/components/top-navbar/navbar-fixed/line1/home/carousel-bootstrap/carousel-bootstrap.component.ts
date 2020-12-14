08bd6332e6cb46c77a363a16eaa4629555d79715
ConstantinAlexuta <constantin.alexuta@gmail.com>
2020-12-14 13:55:30 +0200

    work update

--- src/app/components/top-navbar/navbar-fixed/line1/home/carousel-bootstrap/carousel-bootstrap.component.ts
+++ src/app/components/top-navbar/navbar-fixed/line1/home/carousel-bootstrap/carousel-bootstrap.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselBootstrapComponent implements OnInit {
 
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+ public images2: string[] = [
+    "assets\lgt\images\carousel\bg1.jpg",
+    "assets\lgt\images\carousel\Bora-Bora-1920-x-606.jpg"
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
