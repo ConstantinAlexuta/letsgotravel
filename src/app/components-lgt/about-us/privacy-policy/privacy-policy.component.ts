fbffdd869a11acc512c3a9d434b725493345e706
ConstantinAlexuta <constantin.alexuta@gmail.com>
2020-12-18 15:52:32 +0200

    work update

--- src/app/components-lgt/about-us/privacy-policy/privacy-policy.component.ts
+++ src/app/components-lgt/about-us/privacy-policy/privacy-policy.component.ts
@@ -12,4 +12,13 @@ export class PrivacyPolicyComponent implements OnInit {
   ngOnInit(): void {
   }
 
+        async scrollToTopOfThisElement() {
+        // await this.delay(300);
+        var miliSecondsDelay = 400; // matters on long text content
+        await  new Promise( resolve => setTimeout(resolve, miliSecondsDelay) );
+        var spaceFromTop = document!.getElementById("top_left_navbar_mobile_and_desktop_common")!.getBoundingClientRect().height;
+        // var spaceFromTop = 120;  // depend if is mobile or desktop => hight of navbar
+        window.scrollBy(0, document!.activeElement!.getBoundingClientRect().top - spaceFromTop - 15);
+      }
+
 }
