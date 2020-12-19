94ca37dc3c6026fac637e4b16dc4af832d1e93da
ConstantinAlexuta <constantin.alexuta@gmail.com>
2020-12-19 21:50:35 +0200

    work update

--- src/app/models/offers.ts
+++ src/app/models/offers.ts
@@ -1,9 +1,9 @@
 export const offers = [
   {
-    id: '6353',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/82034793-1.jpg',
+    id: 'offer6353',
+    firstImgSrc: 'assets/lgt/offers/offer6353/firstImgSrc/img.png',
     offer_name: 'ROYALTON RIVIERA CANCUN RESORT & SPA 5* | ALL INCLUSIVE',
-    destination_detail: 'Carretera Federal 307 Tulum-Cancun, Puerto Morelos, Mexic',    
+    destination_detail: 'Carretera Federal 307 Tulum-Cancun, Puerto Morelos, Mexic',
     destination_city_name: 'Cancun',
     destination_type: 'Exotic',
     revelion: '',
@@ -12,10 +12,10 @@ export const offers = [
     lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
   },
   {
-  id: '6354',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/88093319-1-e1607942907577.jpg',
+  id: 'offer6354',
+    firstImgSrc: 'assets/lgt/offers/offer6354/firstImgSrc/img.png',
     offer_name: 'NOW ONYX PUNTA CANA 5* | ALL INCLUSIVE – REVELION 2021',
-    destination_detail: 'Uvero Alto, Punta Cana, Dominican Republic',    
+    destination_detail: 'Uvero Alto, Punta Cana, Dominican Republic',
     destination_city_name: 'Punta Cana',
     destination_type: 'Exotic',
     revelion: 'Revelion',
@@ -24,10 +24,10 @@ export const offers = [
     lgt_http: 'https://lgt.ro/oferta-item/now-onyx-punta-cana-5/'
   },
   {
-  id: '6355',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/129946527-1.jpg',
+  id: 'offer6355',
+    firstImgSrc: 'assets/lgt/offers/offer6355/firstImgSrc/img.png',
     offer_name: 'HARBOR CLUB ST LUCIA, CURIO COLLECTION BY HILTON 4* | ALL INCLUSIVE',
-    destination_detail: 'Rodney Bay, Gros Islet, Saint Lucia',    
+    destination_detail: 'Rodney Bay, Gros Islet, Saint Lucia',
     destination_city_name: 'Saint Lucia',
     destination_type: '',
     revelion: '',
@@ -35,13 +35,11 @@ export const offers = [
     price: 1341,
     lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
   },
-
-  
-    {
-    id: '6353',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/82034793-1.jpg',
+  {
+    id: 'offer6353',
+    firstImgSrc: 'assets/lgt/offers/offer6353/firstImgSrc/img.png',
     offer_name: 'ROYALTON RIVIERA CANCUN RESORT & SPA 5* | ALL INCLUSIVE',
-    destination_detail: 'Carretera Federal 307 Tulum-Cancun, Puerto Morelos, Mexic',    
+    destination_detail: 'Carretera Federal 307 Tulum-Cancun, Puerto Morelos, Mexic',
     destination_city_name: 'Cancun',
     destination_type: 'Exotic',
     revelion: '',
@@ -50,10 +48,10 @@ export const offers = [
     lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
   },
   {
-  id: '6354',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/88093319-1-e1607942907577.jpg',
+  id: 'offer6354',
+    firstImgSrc: 'assets/lgt/offers/offer6354/firstImgSrc/img.png',
     offer_name: 'NOW ONYX PUNTA CANA 5* | ALL INCLUSIVE – REVELION 2021',
-    destination_detail: 'Uvero Alto, Punta Cana, Dominican Republic',    
+    destination_detail: 'Uvero Alto, Punta Cana, Dominican Republic',
     destination_city_name: 'Punta Cana',
     destination_type: 'Exotic',
     revelion: 'Revelion',
@@ -62,10 +60,10 @@ export const offers = [
     lgt_http: 'https://lgt.ro/oferta-item/now-onyx-punta-cana-5/'
   },
   {
-  id: '6355',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/129946527-1.jpg',
+  id: 'offer6355',
+    firstImgSrc: 'assets/lgt/offers/offer6355/firstImgSrc/img.png',
     offer_name: 'HARBOR CLUB ST LUCIA, CURIO COLLECTION BY HILTON 4* | ALL INCLUSIVE',
-    destination_detail: 'Rodney Bay, Gros Islet, Saint Lucia',    
+    destination_detail: 'Rodney Bay, Gros Islet, Saint Lucia',
     destination_city_name: 'Saint Lucia',
     destination_type: '',
     revelion: '',
@@ -73,11 +71,11 @@ export const offers = [
     price: 1341,
     lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
   },
-    {
-    id: '6353',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/82034793-1.jpg',
+  {
+    id: 'offer6353',
+    firstImgSrc: 'assets/lgt/offers/offer6353/firstImgSrc/img.png',
     offer_name: 'ROYALTON RIVIERA CANCUN RESORT & SPA 5* | ALL INCLUSIVE',
-    destination_detail: 'Carretera Federal 307 Tulum-Cancun, Puerto Morelos, Mexic',    
+    destination_detail: 'Carretera Federal 307 Tulum-Cancun, Puerto Morelos, Mexic',
     destination_city_name: 'Cancun',
     destination_type: 'Exotic',
     revelion: '',
@@ -86,10 +84,10 @@ export const offers = [
     lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
   },
   {
-  id: '6354',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/88093319-1-e1607942907577.jpg',
+  id: 'offer6354',
+    firstImgSrc: 'assets/lgt/offers/offer6354/firstImgSrc/img.png',
     offer_name: 'NOW ONYX PUNTA CANA 5* | ALL INCLUSIVE – REVELION 2021',
-    destination_detail: 'Uvero Alto, Punta Cana, Dominican Republic',    
+    destination_detail: 'Uvero Alto, Punta Cana, Dominican Republic',
     destination_city_name: 'Punta Cana',
     destination_type: 'Exotic',
     revelion: 'Revelion',
@@ -98,10 +96,46 @@ export const offers = [
     lgt_http: 'https://lgt.ro/oferta-item/now-onyx-punta-cana-5/'
   },
   {
-  id: '6355',
-    firstImgSrc: 'https://lgt.ro/wp-content/uploads/129946527-1.jpg',
+  id: 'offer6355',
+    firstImgSrc: 'assets/lgt/offers/offer6355/firstImgSrc/img.png',
     offer_name: 'HARBOR CLUB ST LUCIA, CURIO COLLECTION BY HILTON 4* | ALL INCLUSIVE',
-    destination_detail: 'Rodney Bay, Gros Islet, Saint Lucia',    
+    destination_detail: 'Rodney Bay, Gros Islet, Saint Lucia',
+    destination_city_name: 'Saint Lucia',
+    destination_type: '',
+    revelion: '',
+    package_details: 'Pachet St. Lucia - Zbor + Cazare & Masa + Transfer',
+    price: 1341,
+    lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
+  },
+  {
+    id: 'offer6353',
+    firstImgSrc: 'assets/lgt/offers/offer6353/firstImgSrc/img.png',
+    offer_name: 'ROYALTON RIVIERA CANCUN RESORT & SPA 5* | ALL INCLUSIVE',
+    destination_detail: 'Carretera Federal 307 Tulum-Cancun, Puerto Morelos, Mexic',
+    destination_city_name: 'Cancun',
+    destination_type: 'Exotic',
+    revelion: '',
+    package_details: 'Pachet Punta Cana - Zbor + Cazare & Masa + Transfer',
+    price: 1341,
+    lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
+  },
+  {
+  id: 'offer6354',
+    firstImgSrc: 'assets/lgt/offers/offer6354/firstImgSrc/img.png',
+    offer_name: 'NOW ONYX PUNTA CANA 5* | ALL INCLUSIVE – REVELION 2021',
+    destination_detail: 'Uvero Alto, Punta Cana, Dominican Republic',
+    destination_city_name: 'Punta Cana',
+    destination_type: 'Exotic',
+    revelion: 'Revelion',
+    package_details: 'Pachet Punta Cana - Zbor + Cazare & Masa + Transfer',
+    price: 1341,
+    lgt_http: 'https://lgt.ro/oferta-item/now-onyx-punta-cana-5/'
+  },
+  {
+  id: 'offer6355',
+    firstImgSrc: 'assets/lgt/offers/offer6355/firstImgSrc/img.png',
+    offer_name: 'HARBOR CLUB ST LUCIA, CURIO COLLECTION BY HILTON 4* | ALL INCLUSIVE',
+    destination_detail: 'Rodney Bay, Gros Islet, Saint Lucia',
     destination_city_name: 'Saint Lucia',
     destination_type: '',
     revelion: '',
@@ -109,5 +143,4 @@ export const offers = [
     price: 1341,
     lgt_http: 'https://lgt.ro/oferta-item/royalton-riviera-cancun-resort-spa-5/'
   }
-
 ];
